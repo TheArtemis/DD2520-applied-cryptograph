5411afb17385db580eb0c349d3273621fbb5059f
TheArtemis <lordef01@gmail.com>
2026-02-04 18:39:24 +0100

    refactor: change xor_bytes function parameters from Vec<u8> to &[u8] for improved efficiency

--- cryptopals/src/set1/xor.rs
+++ cryptopals/src/set1/xor.rs
@@ -3,12 +3,12 @@ use crate::set1::encoding::{hex_decode, hex_encode};
 pub fn hex_xor(a: &str, b: &str) -> String {
     let a_bytes = hex_decode(a);
     let b_bytes = hex_decode(b);
-    let result = xor_bytes(a_bytes, b_bytes);
+    let result = xor_bytes(&a_bytes, &b_bytes);
 
     hex_encode(&result)
 }
 
-pub fn xor_bytes(a: Vec<u8>, b: Vec<u8>) -> Vec<u8> {
+pub fn xor_bytes(a: &[u8], b: &[u8]) -> Vec<u8> {
     assert_eq!(a.len(), b.len(), "Bytes must be the same length");
     
     let mut result = Vec::with_capacity(a.len());
