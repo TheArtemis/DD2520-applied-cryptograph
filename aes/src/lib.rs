de70252672d66f17430acca5aa94d9a6edf2602c
TheArtemis <lordef01@gmail.com>
2026-02-04 18:54:42 +0100

    refactor: moved ecb aes in separate module

--- aes/src/lib.rs
+++ aes/src/lib.rs
@@ -2,6 +2,8 @@ pub mod state;
 pub mod alg;
 pub mod gf256;
 pub mod sbox;
+pub mod ecb;
 
 pub use state::State;
 pub use alg::AES128;
+pub use ecb::{aes_128_ecb_decrypt, aes_128_ecb_encrypt};
--- cryptopals/tests/set1/challenge7.rs
+++ cryptopals/tests/set1/challenge7.rs
@@ -1,18 +1,7 @@
-use aes::{AES128, State};
+use aes::aes_128_ecb_decrypt;
 use cryptopals::set1::encoding::base64_decode;
 
 const KEY: &[u8] = b"YELLOW SUBMARINE";
-
-fn aes_128_ecb_decrypt(cipher_text: &[u8], key: &[u8; 16]) -> Vec<u8> {
-    let aes = AES128::new(*key);
-    let mut plaintext = Vec::with_capacity(cipher_text.len());
-    for block in cipher_text.chunks_exact(16) {
-        let mut state = State::new(block.try_into().unwrap());
-        aes.inv_cipher(&mut state);
-        plaintext.extend_from_slice(state.as_bytes());
-    }
-    plaintext
-}
 
 #[test]
 fn test_challenge7_decrypt_aes_ecb() {
